06da3f6bc118f309ef4ab2aec35255a6fddd3028
cody-chen-jf <459741515@qq.com>
2019-09-26 10:07:02 +0800

    npm update

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -2,11 +2,15 @@
   <f7-page>
     <f7-navbar>
       <f7-nav-left>
-        <f7-link icon-if-ios="f7:menu" icon-if-md="material:menu" panel-open="left"></f7-link>
+        <f7-link panel-open="left">
+          <i class="f7-icons">menu</i>
+        </f7-link>
       </f7-nav-left>
       <f7-nav-title>My App</f7-nav-title>
       <f7-nav-right>
-        <f7-link icon-if-ios="f7:menu" icon-if-md="material:menu" panel-open="right"></f7-link>
+        <f7-link panel-open="right">
+          <i class="f7-icons">menu</i>
+        </f7-link>
       </f7-nav-right>
     </f7-navbar>
     <f7-toolbar bottom>
